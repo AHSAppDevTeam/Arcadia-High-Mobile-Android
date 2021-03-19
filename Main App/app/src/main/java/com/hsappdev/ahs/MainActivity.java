ba4aef1fb72a104158b116beba18bed57d776fd4
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2021-03-19 15:16:39 -0700

    Navbar with default icons

--- Main App/app/src/main/java/com/hsappdev/ahs/MainActivity.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null); // Remove tint from navbar; Required for navbar icons to work
     }
 }
