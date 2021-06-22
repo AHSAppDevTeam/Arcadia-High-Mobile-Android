bc37b6341a9051a55af80ae8fb68ecc7d6bd2942
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-22 05:17:11 -0700

    Student ID barcode implementation
    Draws black rectangular stripes on a ShapeDrawable inside the barcode ImageView to produce a Code 39 barcode.

--- Main App/app/src/main/java/com/hsappdev/ahs/UI/profile/ProfileCardFragment.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/UI/profile/ProfileCardFragment.java
@@ -12,10 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.hsappdev.ahs.R;
+import com.hsappdev.ahs.util.BarcodeDrawable;
 import com.hsappdev.ahs.util.ImageUtil;
-import com.hsappdev.ahs.util.ScreenUtil;
-
-import org.w3c.dom.Text;
 
 public class ProfileCardFragment extends Fragment {
     private static final String TAG = "ProfileCardFragment";
@@ -28,70 +26,62 @@ public class ProfileCardFragment extends Fragment {
 
     private TextView firstNameTextView;
     private TextView lastNameTextView;
-    private ImageView barcode;
+    private ImageView barcodeImage;
     private ImageView accountImage;
-
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.profile_card_fragment, container, false);
         // Set vars
-        firstNameTextView = view.findViewById(R.id.profile_card_firstname_textView);
-        lastNameTextView = view.findViewById(R.id.profile_card_lastname_textView);
-        barcode = view.findViewById(R.id.profile_card_barcode);
+        firstNameTextView = view.findViewById(R.id.profile_card_first_name_textView);
+        lastNameTextView = view.findViewById(R.id.profile_card_last_name_textView);
+        barcodeImage = view.findViewById(R.id.profile_card_barcode_img);
         accountImage = view.findViewById(R.id.profile_card_profile_img);
 
+        setDetails("Giovanna-Xiomara","Carranza-Castellon","https://ahs.app/icon.png",69420);
         return view;
     }
 
-    public void renderBarcode(){
-        // TODO: use the userId and render a barcode
-    }
 
     // GETTERS AND SETTERS
     public void setDetails(String firstName, String lastName, String profilePictureUrl, int userId){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.profilePictureUrl = profilePictureUrl;
-        this.userId = userId;
-
-        // Update View
-        firstNameTextView.setText(firstName);
-        lastNameTextView.setText(lastName);
-        ImageUtil.setImageToView(profilePictureUrl, accountImage);
-        renderBarcode();
+        setFirstName(firstName);
+        setLastName(lastName);
+        setProfilePictureUrl(profilePictureUrl);
+        setUserId(userId);
     }
 
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+        firstNameTextView.setText(firstName);
     }
 
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
+        lastNameTextView.setText(lastName);
     }
 
     public String getProfilePictureUrl() {
         return profilePictureUrl;
     }
-
     public void setProfilePictureUrl(String profilePictureUrl) {
         this.profilePictureUrl = profilePictureUrl;
+        ImageUtil.setImageToView(profilePictureUrl, accountImage);
     }
 
     public int getUserId() {
         return userId;
     }
-
     public void setUserId(int userId) {
         this.userId = userId;
+        BarcodeDrawable barcodeCanvas = new BarcodeDrawable(userId);
+        barcodeImage.setImageDrawable(barcodeCanvas);
     }
 }
