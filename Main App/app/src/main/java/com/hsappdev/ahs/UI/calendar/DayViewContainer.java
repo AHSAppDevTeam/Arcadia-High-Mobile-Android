0d3e0ca3fdb7b1a7330b57958409667fc4a96f35
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2021-09-03 21:54:10 -0700

    Improved Calendar Rendering

--- Main App/app/src/main/java/com/hsappdev/ahs/UI/calendar/DayViewContainer.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/UI/calendar/DayViewContainer.java
@@ -1,5 +1,6 @@
 package com.hsappdev.ahs.UI.calendar;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import com.hsappdev.ahs.UI.calendar.calendarBackend.Day;
 import com.hsappdev.ahs.UI.calendar.calendarBackend.Schedule;
 import com.hsappdev.ahs.UI.calendar.newCalendar.CalendarBackendNew;
 import com.kizitonwose.calendarview.model.CalendarDay;
+import com.kizitonwose.calendarview.model.DayOwner;
 import com.kizitonwose.calendarview.ui.ViewContainer;
 
 import org.jetbrains.annotations.NotNull;
@@ -27,23 +29,36 @@ public class DayViewContainer extends ViewContainer implements CalendarDayLoadCa
     private static final String TAG = "DayViewContainer";
 
     private TextView dayText;
+    private TextView dayInfo;
     private LocalDate date;
 
 
     public DayViewContainer(@NotNull View view) {
         super(view);
         dayText = view.findViewById(R.id.calendarDayText);
+        dayInfo = view.findViewById(R.id.calendarDayInfo);
 
     }
 
     public void updateView(CalendarDay calendarDay) {
         date = calendarDay.getDate();
+        dayText.setText(Integer.toString(calendarDay.getDay()));
+        if(calendarDay.getOwner() == DayOwner.THIS_MONTH){
+            dayText.setTextColor(Color.BLACK);
+            if(calendarDay.getDay() == getDayOfMonth()) {
+               dayText.setBackgroundColor(Color.YELLOW);
+            } else {
+                dayText.setBackgroundColor(Color.LTGRAY);
+            }
+        } else {
+            dayText.setTextColor(Color.GRAY);
+        }
         CalendarBackendNew.getInstance().registerForCallback(getWeekOfYear(), getDayOfWeek(), this);
     }
 
 
     public void onCalendarDayLoad(Day requestedDate) {
-        dayText.setText(requestedDate.getScheduleId());
+        //dayText.setText(requestedDate.getScheduleId());
         requestedDate.loadSchedule(this);
     }
 
@@ -64,8 +79,13 @@ public class DayViewContainer extends ViewContainer implements CalendarDayLoadCa
         return date.getDayOfWeek().getValue();
     }
 
+    private int getDayOfMonth() {
+        return LocalDate.now().getDayOfMonth();
+    }
+
     @Override
     public void onCalendarScheduleLoad(Schedule schedule) {
-        dayText.setText(schedule.getIconURL());
+        dayInfo.setBackgroundColor(Color.parseColor(schedule.getColor()));
+        dayInfo.setText(schedule.getTitle());
     }
 }
