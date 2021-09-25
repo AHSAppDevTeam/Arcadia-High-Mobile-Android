a21de30b8f2bc5c81b15a0c3294a37858ae7ba5d
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2021-09-25 13:45:53 -0700

    Start Implementing Schedule Rendering

--- Main App/app/src/main/java/com/hsappdev/ahs/UI/calendar/newCalendar/ScheduleRenderer.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/UI/calendar/newCalendar/ScheduleRenderer.java
@@ -1,10 +1,12 @@
 package com.hsappdev.ahs.UI.calendar.newCalendar;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.hsappdev.ahs.R;
 import com.hsappdev.ahs.UI.calendar.calendarBackend.Schedule;
 
 import org.w3c.dom.Text;
@@ -30,16 +32,23 @@ public class ScheduleRenderer {
         for (int i = 0; i < schedule.getTimestamps().size(); i+=2) {
             int timestampStart = schedule.getTimestamps().get(i);
             int timestampEnd = schedule.getTimestamps().get(i+1);
-            TextView timestampText = new TextView(view.getContext());
-            timestampText.setText(String.format("Period %d %s to %s", i / 2 + 1 , getDisplayTime(timestampStart), getDisplayTime(timestampEnd)));
-            view.addView(timestampText);
+            int periodNum = i / 2 + 1;
+            view.addView(generateScheduleBubbleView(timestampStart, timestampEnd, periodNum, view));
         }
     }
 
     private String getDisplayTime(int timestampStart) {
         int hour = timestampStart/60;
         String AMvsPM = "AM";
-        if(hour > 12) {hour -= 12; AMvsPM = "PM";}
+        if(hour >= 12) {hour -= 12; AMvsPM = "PM";}
         return String.format("%d:%02d %s", hour, timestampStart%60, AMvsPM);
+    }
+
+    private View generateScheduleBubbleView(int timestampStart, int timestampEnd, int periodNum, View view){
+        LayoutInflater layoutInflater = LayoutInflater.from(view.getContext());
+        View bubble = layoutInflater.inflate(R.layout.schedule_period_bubble, null, false);
+        TextView timestampText = bubble.findViewById(R.id.schedule_period_bubble_text);
+        timestampText.setText(String.format("Period %d %s to %s", periodNum, getDisplayTime(timestampStart), getDisplayTime(timestampEnd)));
+        return bubble;
     }
 }
