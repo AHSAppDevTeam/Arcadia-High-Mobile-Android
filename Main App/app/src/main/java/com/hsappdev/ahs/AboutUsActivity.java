2f63f794ad8075f5d2ff634370bb95644713bd6b
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2021-10-30 18:50:59 -0700

    Finish About Us Page

--- Main App/app/src/main/java/com/hsappdev/ahs/AboutUsActivity.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/AboutUsActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.hsappdev.ahs.util.ScreenUtil;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        TextView programmers = findViewById(R.id.programmers_list);
+        ScreenUtil.setHTMLStringToTextView(getResources().getString(R.string.programmers_list), programmers);
+
+        TextView graphicDesigners = findViewById(R.id.graphic_designers_list);
+        ScreenUtil.setHTMLStringToTextView(getResources().getString(R.string.graphic_designers_list), graphicDesigners);
+
+        TextView contentEditors = findViewById(R.id.content_editors_list);
+        ScreenUtil.setHTMLStringToTextView(getResources().getString(R.string.content_editors_list), contentEditors);
 
         ImageButton homeButton = findViewById(R.id.about_us_activity_home_button);
         homeButton.setOnClickListener(new View.OnClickListener() {
