947c3872d80d7d9077995e2811b23a31c362fa4d
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2022-06-25 01:13:49 -0700

    Base nfc test

---
+++ New-Projects/MigratedApp/app/src/main/java/com/hsappdev/ahs/nfcTest/NfcTestActivity.java
@@ -0,0 +1,16 @@
+package com.hsappdev.ahs.nfcTest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hsappdev.ahs.R;
+
+public class NfcTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_test);
+    }
+}
