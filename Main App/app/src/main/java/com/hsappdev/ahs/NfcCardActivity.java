bc877c335a8b2738b3351eb6ff34e3278a170af0
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2022-08-09 14:46:03 -0700

    Commit changes

--- Main App/app/src/main/java/com/hsappdev/ahs/NfcCardActivity.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/NfcCardActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.hsappdev.ahs.UI.profile.ProfileCardFragment;
+
 public class NfcCardActivity extends AppCompatActivity {
+
+    private ProfileCardFragment profileCardFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_card);
+
+        profileCardFragment = new ProfileCardFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.profileCardFragmentHolder, profileCardFragment)
+                .commit();
     }
 }
