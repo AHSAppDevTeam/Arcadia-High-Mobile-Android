18d409c8e2c3f7a9b07a19e531a282a28e7124dc
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2022-09-15 13:14:20 -0700

    Update version

--- Main App/app/src/main/java/com/hsappdev/ahs/localdb/RoomDatabase.java
+++ Main App/app/src/main/java/com/hsappdev/ahs/localdb/RoomDatabase.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Article.class, Category.class, CategoryList.class}, version = 7, exportSchema = false)
+@Database(entities = {Article.class, Category.class, CategoryList.class}, version = 8, exportSchema = false)
 @TypeConverters(Converters.class)
 public abstract class RoomDatabase extends androidx.room.RoomDatabase {
 
